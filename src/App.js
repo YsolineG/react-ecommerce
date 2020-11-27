46d5c79aadea67c2941bee4ac8ea5b7bf66d94d2
YsolineG <ysoline.ganster@gmail.com>
2020-11-27 08:33:28 +0100

    faet: edit gamesList

--- src/App.js
+++ src/App.js
@@ -5,34 +5,34 @@ import { BrowserRouter } from "react-router-dom";
 import ShoppingBag from "./ShoppingBag.js";
 import { Switch, Route } from "react-router";
 
-// const gamesList = [
-//   {
-//     id: 1,
-//     image:
-//       "https://www.actugaming.net/wp-content/uploads/2020/08/fall-guys3.jpg",
-//     name: "Fall Guys",
-//     description: "Fall Guys: Ultimate Knockout, soit le dernier survivant.",
-//     price: "20€",
-//     category: {
-//       id: 1,
-//       name: "PC",
-//       slug: "pc",
-//     },
-//   },
-//   {
-//     id: 2,
-//     image:
-//       "http://ubistatic19-a.akamaihd.net/ubicomstatic/fr-fr/global/search-thumbnail/ubicom-jd2021-page-meta-artwork_mobile_363097.jpg",
-//     name: "Just Dance 2021",
-//     description: "Jeu de danse par excellence avec 40 nouveaux tubes",
-//     price: "60€",
-//     category: {
-//       id: 2,
-//       name: "Switch",
-//       slug: "nintendo-switch",
-//     },
-//   },
-// ];
+const gamesList = [
+  {
+    id: 1,
+    image:
+      "https://www.actugaming.net/wp-content/uploads/2020/08/fall-guys3.jpg",
+    name: "Fall Guys",
+    description: "Fall Guys: Ultimate Knockout, soit le dernier survivant.",
+    price: "20€",
+    category: {
+      id: 1,
+      name: "PC",
+      slug: "pc",
+    },
+  },
+  {
+    id: 2,
+    image:
+      "http://ubistatic19-a.akamaihd.net/ubicomstatic/fr-fr/global/search-thumbnail/ubicom-jd2021-page-meta-artwork_mobile_363097.jpg",
+    name: "Just Dance 2021",
+    description: "Jeu de danse par excellence avec 40 nouveaux tubes",
+    price: "60€",
+    category: {
+      id: 2,
+      name: "Switch",
+      slug: "nintendo-switch",
+    },
+  },
+];
 
 const productsList = [
   {
